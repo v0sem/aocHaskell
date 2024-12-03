c2eb593fe9390fee138defb8b6ec3ef60435db9d
v0sem <progpabsanch@gmail.com>
2024-12-03 13:35:55 +0100

    First steps

--- app/Days/Y2024/Day2.hs
+++ app/Days/Y2024/Day2.hs
@@ -1,1 +1,12 @@
 module Days.Y2024.Day2 where
+
+import Data.List.Split (splitOn)
+
+parseLine :: String -> [Int]
+parseLine line = map (\x -> read x) splitOn " " line
+
+safe :: String -> Bool
+safe = True
+
+day :: [String] -> Int
+day inputLines = sum (map (\x -> if safe x then 1 else 0) inputLines)
