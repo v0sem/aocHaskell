622eb3f384592d8002db1a2e9b93a1a2ef8c1e03
v0sem <progpabsanch@gmail.com>
2024-12-04 19:07:03 +0100

    Started things that are just weird

---
+++ app/Days/Y2024/Day4.hs
@@ -0,0 +1,14 @@
+module Days.Y2024.Day4 where
+
+scanLine :: String -> Int -> [Int]
+scanLine line col = [0]
+
+scanMatrix :: [String] -> Int -> [Int]
+scanMatrix matrix row
+  | (x : xs) row = [sum (scanLine x 0), sum (scanMatrix xs (row + 1))]
+
+day :: [String] -> Int
+day _ = 0
+
+dayTwo :: [String] -> Int
+dayTwo _ = 0
