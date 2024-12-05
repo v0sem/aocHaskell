4dc92d8d06bfe69ddc4a97d21d2cad9ab7970330
v0sem <progpabsanch@gmail.com>
2024-12-05 16:42:16 +0100

    Started 4

--- app/Days/Y2024/Day4.hs
+++ app/Days/Y2024/Day4.hs
@@ -1,11 +1,8 @@
 module Days.Y2024.Day4 where
 
-scanLine :: String -> Int -> [Int]
-scanLine line col = [0]
+isXMAS :: [String] -> String -> (Int, Int) -> (Int, Int) -> Bool
 
-scanMatrix :: [String] -> Int -> [Int]
-scanMatrix matrix row
-  | (x : xs) row = [sum (scanLine x 0), sum (scanMatrix xs (row + 1))]
+isXMAS matrix "Start" (x, y) (dirx, diry)
 
 day :: [String] -> Int
 day _ = 0
