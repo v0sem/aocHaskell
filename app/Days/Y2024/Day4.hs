0cb67bdb2da478f78931df10691c8bc12aeba49d
v0sem <progpabsanch@gmail.com>
2024-12-05 12:12:40 +0100

    Day 3 done and yassified

--- app/Days/Y2024/Day4.hs
+++ app/Days/Y2024/Day4.hs
@@ -1,11 +1,11 @@
 module Days.Y2024.Day4 where
 
-scanLine :: String -> Int -> [Int]
-scanLine line col = [0]
+--scanLine :: String -> Int -> [Int]
+--scanLine line col = [0]
 
-scanMatrix :: [String] -> Int -> [Int]
-scanMatrix matrix row
-  | (x : xs) row = [sum (scanLine x 0), sum (scanMatrix xs (row + 1))]
+--scanMatrix :: [String] -> Int -> [Int]
+--scanMatrix matrix row
+  -- | (x : xs) row = [sum (scanLine x 0), sum (scanMatrix xs (row + 1))]
 
 day :: [String] -> Int
 day _ = 0
